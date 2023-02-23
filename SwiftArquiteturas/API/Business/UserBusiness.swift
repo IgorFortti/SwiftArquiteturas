dc090b129d6547ccb09a905b0570b9108a7fad41
Igor Fortti <igormfortti@gmail.com>
2023-02-23 14:16:20 -0300

    Criacao da classe UserBusiness

---
+++ SwiftArquiteturas/API/Business/UserBusiness.swift
@@ -0,0 +1,46 @@
+//
+//  UserBusiness.swift
+//  SwiftArquiteturas
+//
+//  Created by Igor Fortti on 23/02/23.
+//
+// A classe UserBusiness vai decidir qual dos provider ele vai chamar.
+import Foundation
+
+protocol UserBusinessProtocol {
+    func login(email: String, password: String, completionHandler: @escaping(Result<UserModel, Error>) -> Void)
+    func register(email: String, password: String, completionHandler: @escaping(Result<UserModel, Error>) -> Void)
+}
+
+class UserBusiness: UserBusinessProtocol {
+    let provider: UserProvider = UserProvider()
+    
+    func login(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        provider.login(parameters: getParams(email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHandler(.success(userModel))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+        
+    }
+    
+    func register(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        provider.register(parameters: getParams(email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHandler(.success(userModel))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+    
+    private func getParams(_ email: String, _ password: String) -> [AnyHashable: Any] {
+        let userModel = UserModel(email: email, password: password)
+        let params: [AnyHashable: Any] = [Constants.ParametersKeys.body: [Constants.ParametersKeys.userModel: userModel]]
+        return params
+    }
+}
