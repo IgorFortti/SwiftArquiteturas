ceafbf611062aea07c4a4dd3b0ebd7fec7d43513
Igor Fortti <igormfortti@gmail.com>
2023-02-24 11:32:19 -0300

    Criacao de HomeViewController, LoginViewController, RegisterViewController e ligacao das outlets e das actions

---
+++ SwiftArquiteturas/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  SwiftArquiteturas
+//
+//  Created by Igor Fortti on 24/02/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func openButtonTap(_ sender: Any) {
+    }
+    
+    @IBAction func registerButtonTap(_ sender: Any) {
+    }
+    
+
+
+}
---
+++ SwiftArquiteturas/Controller/RegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterViewController.swift
+//  SwiftArquiteturas
+//
+//  Created by Igor Fortti on 24/02/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func registerButtonTap(_ sender: Any) {
+    }
+    
+    @IBAction func openButtonTap(_ sender: Any) {
+    }
+    
+
+
+}
